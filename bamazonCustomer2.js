d2563ec9ff9b87e32d340ee158c9188467360de5
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-05-13 00:34:35 -0400

    order now prints after user selects quantity

--- bamazonCustomer2.js
+++ bamazonCustomer2.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+var userIDChosen;
 
 // connection to mySQL
 var connection = mysql.createConnection({
@@ -8,74 +8,92 @@ var connection = mysql.createConnection({
     user     : 'root',
     password : 'Gators14!',
     database : 'bamazon_db'
-  });
-  
-  // if error connecting to mySQL
-  connection.connect(function(err) {
+});
+
+// if error connecting to mySQL
+connection.connect(function(err) {
     if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+        console.error('error connecting: ' + err.stack);
+        return;
     }
     
     //displays mySQL connection ID
     console.log('connected as id ' + connection.threadId);
-
+    
     //function to display everything for purchase
     function initialConnect() {
+        
+        connection.query('SELECT * FROM products', function (error, results, fields) {
+            
+            myChoices = results.map(element => {
+                return (element.item_id + ": " + element.product_name + ": " + element.price);
+            })
+            console.log(myChoices);
+            
+            orderRequested(results);
+            
+            // then put it in choices of list type
+            
+         //   connection.end();
+            
+        });}
+        
+        //calling initial connect function
+        initialConnect();})
+        
+        function orderRequested(dbItems) {
+            
+            inquirer
+            .prompt([{
+                type: "list",
+                name: "choose_item",
+                message: "What is the ID of the product you would like to buy?",
+                choices: myChoices
+            }, {
+                type: "input",
+                name: "choose_amount",
+                message: "what quantity would you like?"
+            }
+            /* Pass your questions in here */
+        ])
+        
+        .then(function(answer) {
 
-    connection.query('SELECT * FROM products', function (error, results, fields) {
+            let itemChosenID;
+            
+           userIDChosen = answer.choose_item.split(":")[0];
+           //console.log("ID of chosen choice: " + userIDChosen)
+           
+           dbItems.forEach((element, index) => {
+               if (element.item_id == userIDChosen) { itemChosenID = element.item_id - 1;
+        console.log("Item selected: " + itemChosenID);
+        }})
 
-myChoices = results.map(element => {
-  return (element.item_id + ": " + element.product_name + ": " + element.price);
-})
-console.log(myChoices);
+        connection.query('SELECT stock_quantity FROM products', function (error, results, fields) {
+            if (error) throw error;
 
-orderRequested();
+            console.log(results[itemChosenID]);
+            var customerQuantity = parseInt(answer.choose_amount)
+            console.log("Customer quantity: " + customerQuantity);
+            var dbItemQuantity = parseInt(results[itemChosenID].stock_quantity);
+            console.log("Warehouse quantity: " + dbItemQuantity);
+            let stockRemaining = dbItemQuantity - customerQuantity;
+            console.log("Stock remaining: " + stockRemaining);
+            if (dbItemQuantity >= customerQuantity) {
+                console.log("We have your items in stock. Please wait while we process your order.") 
+            } else {
+                console.log("Sorry, we're currently out of stock.")
+                return 
+            }
+            
+        })
 
-// then put it in choices of list type
-       /* for (var i = 0; i < results.length; i++) {
-            console.log("Item ID: " + results[i].item_id + " | " + "Item: " + results[i].product_name + " | " + "Item price: " + results[i].price);
-          }
-          console.log("-----------------------------------"); */
-
-          connection.end();
-
-       });}
-
-       initialConnect();})
-
-       function orderRequested() {
-         
-        inquirer
-        .prompt([{
-          type: "list",
-          name: "choose_item",
-          message: "What is the ID of the product you would like to buy?",
-          choices: myChoices
-         }, {
-           type: "input",
-           name: "choose_amount",
-           message: "what quantity would you like?"
-         }
-         /* Pass your questions in here */
-       ])
-
-       .then(function(answer) {
-         var query = "SELECT * FROM products WHERE item_id = '7'";
-         connection.query(query, {choose_item: answer.choose_item}, function (err, res) {
-           console.log(res.product_name[i])
-         })
-
-         console.log("made it here");
-
-       })}
-
-/* like the db query
-let queryResult = [{id: results.item_id, name: results.product_name, results: results.price}]
-
-myChoices = queryResult.map(element => {
-  return (element.id + ": " + element.name + ": " + element.results);
-})
-
-// then put it in choices of list type
-console.log(myChoices);*/
+        /*
+        var quantityOfItemPicked = element.stock_quantity;
+        if (quantityOfItemPicked >= answer.choose_amount) {
+            console.log("Your item is in stock.");
+        }
+        else {
+            console.log("Sorry, out of stock.");
+        }*/
+    })}
